1ef58c16481540387ce49a42875c7f6cae9df9c1
Billy <uyamb@usf.edu>
2026-06-26 19:47:33 -0400

    main styling complete

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+  return (
+    <main className="min-h-screen bg-gray-100 px-6 py-10">
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+
+        
+      </div>
+    </main>
+  )
+}
