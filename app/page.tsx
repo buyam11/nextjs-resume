19fe5dfcc3c8bf11a6997f115cb0568956b0d09b
Billy <uyamb@usf.edu>
2026-06-26 19:52:14 -0400

    header content added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,20 @@ export default function Resume() {
     <main className="min-h-screen bg-gray-100 px-6 py-10">
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
 
-        
+        {/* Header */}
+        <header>
+          <h1>Billy Joe Uyam</h1>
+
+          <div>
+            <p>billyuyam11@gmail.com</p>
+            <p>(813) 607-7031</p>
+            <a
+              href="linkedin.com/in/billy-u-9a6399382"
+            >
+              LinkedIn Profile
+            </a>
+          </div>
+        </header>
       </div>
     </main>
   )
