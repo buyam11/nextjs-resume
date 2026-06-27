df4fc5bc74dde6feb99a0384ec915900d0f57bca
Billy <uyamb@usf.edu>
2026-06-26 20:29:44 -0400

    education styling added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Resume() {
   return (
     <main className="min-h-screen bg-gray-100 px-6 py-10">
-      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+      <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
 
         {/* Header */}
         <header className="bg-gray-900 text-white px-8 py-8">
-          <h1 className="text-3xl font-bold">Billy Joe Uyam</h1>
+          <h1 className="text-4xl font-bold">Billy Joe Uyam</h1>
 
           <div className="mt-2 text-gray-300 text-sm space-y-1">
             <p>billyuyam11@gmail.com</p>
@@ -23,7 +23,7 @@ export default function Resume() {
 
           {/* Objective */}
           <section>
-            <h2 className="text-xl font-bold uppercase tracking-wide border-b-2 border-gray-30 pb-2"
+            <h2 className="text-xl font-bold uppercase tracking-wide border-b-2 border-gray-300 pb-2"
             >Objective
             </h2>
 
@@ -34,30 +34,30 @@ export default function Resume() {
           </section>
 
           {/* Education*/}
-          <section>
-            <h2
+          <section className="mt-8">
+            <h2 className="text-xl font-bold uppercase tracking-wide border-b-2 border-gray-300 pb-2"
             >Education
             </h2>
 
-            <div>
-              <div>
+            <div className="mt-4">
+              <div className="flex justify-between font-semibold">
                 <h3>University of South Florida</h3>
                 <span>Tampa, FL</span>
               </div>
 
-              <div>
+              <div className="flex justify-between text-gray-700">
                 <p>B.S. Business Analytics and Information Systems</p>
                 <p>Expected May 2028</p>
               </div>
             </div>
 
-            <div>
-              <div>
+            <div className="mt-5">
+              <div className="flex justify-between font-semibold">
                 <h3>State College of Florida</h3>
                 <span>Venice, FL</span>
               </div>
 
-              <div>
+              <div className="flex justify-between text-gray-700">
                 <p>Associate of Arts</p>
                 <p>Aug 2023 - May 2025</p>
               </div>
